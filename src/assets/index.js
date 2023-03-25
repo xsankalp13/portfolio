719e38bbb918d63fdda2df6ed7b34556efef0fba
xsankalp13 <sankalpbsalve@gmail.com>
2023-03-25 12:21:49 +0530

    Until now I have finished work and experience Section

--- src/assets/index.js
+++ src/assets/index.js
@@ -25,6 +25,8 @@ import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
+import GZ from "./company/GZ.png"
+import ApnaStore from './company/ApnaStore.png'
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -56,6 +58,8 @@ export {
   shopify,
   starbucks,
   tesla,
+  ApnaStore,
+  GZ,
   carrent,
   jobit,
   tripguide,
